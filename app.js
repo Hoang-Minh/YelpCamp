d1771863fb108cbe55b51046ce30db5558f6c097
Hoang-Minh <quataoxanh83@gmail.com>
2019-06-25 20:48:34 -0700

    fix upload image

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-var express = require("express"),
+const express = require("express"),
   app = express(),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser"),
@@ -16,10 +16,14 @@ var express = require("express"),
   
 
 //seedDb();
-mongoose.connect("mongodb://localhost/yelp_camp", {
+mongoose.connect("mongodb+srv://admin:"  + process.env.MONGODB_ATLAS + "@cluster0-9fz7j.mongodb.net/test?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true
+}).then(() => {
+  console.log("Connected to DB");
+}).catch(err => {
+  console.log("Error:", err.message);
 });
 
 app.use(express.static(__dirname + "/public"));
@@ -46,7 +50,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
@@ -60,6 +64,6 @@ app.use("/users", userRoutes);
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log("Server is listening at port " + port);
 });
