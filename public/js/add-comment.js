c2c782340d0e3c3cb8e8a8ad5f64e4a3879fa25a
Hoang-Minh <quataoxanh83@gmail.com>
2019-11-21 22:43:55 -0800

    add comment on the campground page

--- public/js/add-comment.js
+++ public/js/add-comment.js
@@ -1,42 +1,16 @@
 $(function(){
-    $("#add-comment").on("click", function(event){    
-        if(!$("#comment").length){
-            let comment = `
-                <div class="form-group">
-                    <textarea class="form-control" rows="5" placeholder="Description" id="comment" name="comment[text]" autofocus></textarea>
-                </div>
-                ` ;
+    // hide the comment when loading
+    $("#comment").toggle();
 
-            $("#submit-comment").prepend(comment);
-            event.preventDefault();
-        } else if ($("#comment").length && $.trim($("#comment").val())){
+    $("#add-comment").on("click", function(event){
+
+        if($("#comment").is(":visible") && $.trim($("#comment").val())){
+            // comment text is not empty
             $("#add-comment").submit();
         } else {
+            // if element is hidden
+            $("#comment").toggle();
             event.preventDefault();
         }
-
-
-        
-        
-
-        // if($("#comment").length){
-        //     if($.trim($("#comment").val())){
-        //         console.log("not empty???");
-        //         $("add-comment").submit();
-        //     } else {
-        //         console.log("empty");
-        //     };
-        // } else {
-        //     let comment = `
-        //     <div class="form-group">
-        //         <textarea class="form-control" rows="5" placeholder="Description" id="comment" name="comment[text]"></textarea>
-        //     </div>
-        //     ` ;
-        //     let t = $("#add-comment");
-            
-        //     $(comment).insertBefore(t.parent());
-
-        // }
-        
     });
 });
