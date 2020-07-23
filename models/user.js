243302593a105ed63dfe410dfb7127c2d786f8ee
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-23 11:36:28 -0700

    change var to const

--- models/user.js
+++ models/user.js
@@ -3,37 +3,37 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-var UserSchema = new mongoose.Schema({
-    username: {type: String, unique: true, required: true},
-    password: String,
-    isAdmin: {type: Boolean, default: false},
-    avatar: String,
-    firstName: String,
-    lastName: String,
-    email: {type: String, unique: true, required: true},
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-    registerToken: String,
-    registerTokenExpires: Date,
-    isVerified: {type: Boolean, default: false},
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: String,
+  isAdmin: { type: Boolean, default: false },
+  avatar: String,
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  registerToken: String,
+  registerTokenExpires: Date,
+  isVerified: { type: Boolean, default: false },
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
-module.exports.hashPassword = async(password) => {
-    try{
-        const salt = await bcrypt.genSalt(saltRounds);
-        return bcrypt.hash(password, salt);
-    } catch (error){
-        throw new Error("Hasing failed: " + error);
-    }
+module.exports.hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    return bcrypt.hash(password, salt);
+  } catch (error) {
+    throw new Error("Hasing failed: " + error);
+  }
 };
 
 module.exports.comparePassword = async (inputPassword, hasedPassword) => {
-    try {
-        return await bcrypt.compare(inputPassword, hasedPassword);
-    } catch (error) {
-        throw new Error('Comparing failed', error);
-    }
+  try {
+    return await bcrypt.compare(inputPassword, hasedPassword);
+  } catch (error) {
+    throw new Error("Comparing failed", error);
+  }
 };
